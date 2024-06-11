a0bc522a62ef1ed45ad7aa7ff6f3a54f65670aa8
salimmghari <mghari.963@gmail.com>
2024-06-11 17:32:11 +0100

    .

--- ios/Notes/Views/HomeView.swift
+++ ios/Notes/Views/HomeView.swift
@@ -1,15 +1,15 @@
 import SwiftUI
 
 struct HomeView: View {
-    @State private var notes: [Note] = []
-    @State private var newNote: Note = Note(
-        id: 0,
-        title: "",
-        body: "",
-        user_id: 0,
-        created_at: "",
-        updated_at: ""
-    )
+//    @State private var notes: [Note] = []
+//    @State private var newNote: Note = Note(
+//        id: 0,
+//        title: "",
+//        body: "",
+//        user_id: 0,
+//        created_at: "",
+//        updated_at: ""
+//    )
     @State private var showAuthView = false
     
     var body: some View {
@@ -17,30 +17,30 @@ struct HomeView: View {
             AppTitle(
                 title: "Notes"
             )
-            ScrollView {
-                ForEach(self.notes) { note in
-                    AppNote(
-                        title: note.title,
-                        body_: note.body,
-                        onTitleChange: {value in},
-                        onBodyChange: {value in},
-                        onUpdate: {},
-                        onDelete: {}
-                    )
-                }
-            }
-            AppNote(
-                title: self.newNote.title,
-                body_: self.newNote.body,
-                onTitleChange: { value in
-                    self.newNote.title = value
-                },
-                onBodyChange: { value in
-                    self.newNote.body = value
-                },
-                onCreate: {},
-                isNew: true
-            )
+//            ScrollView {
+//                ForEach(self.notes) { note in
+//                    AppNote(
+//                        title: note.title,
+//                        body_: note.body,
+//                        onTitleChange: {value in},
+//                        onBodyChange: {value in},
+//                        onUpdate: {},
+//                        onDelete: {}
+//                    )
+//                }
+//            }
+//            AppNote(
+//                title: self.newNote.title,
+//                body_: self.newNote.body,
+//                onTitleChange: { value in
+//                    self.newNote.title = value
+//                },
+//                onBodyChange: { value in
+//                    self.newNote.body = value
+//                },
+//                onCreate: {},
+//                isNew: true
+//            )
         }
             .padding()
             .onAppear() {
